f05b97fc92584f140f322a4c756a265cf1967706
Malek Kazal <ghazal.malek@gmail.com>
2024-05-07 22:51:10 -0400

    React router - context - layouts

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,26 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Users from "./pages/Users";
 import NotFound from "./pages/NotFound";
-import App from "./App";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
     {
-        index: "/",
-        element: <App />,
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/users" />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+            {
+                path: "/users",
+                element: <Users />,
+            },
+        ],
     },
     {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/signup",
-        element: <Signup />,
-    },
-    {
-        path: "/users",
-        element: <Users />,
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ],
     },
     {
         path: "*",
